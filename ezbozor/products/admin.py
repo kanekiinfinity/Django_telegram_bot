a36eb5925ffdbb685e9d90ed8a695e7a7974688e
Grim Reaper <148218228+kanekiinfinity@users.noreply.github.com>
2024-01-21 18:48:05 +0500

    .

---
+++ ezbozor/products/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import User, Product
+
+admin.site.register(User)
+admin.site.register(Product)
+
+# Register your models here.
+
---
+++ handlers/users/__init__.py
@@ -0,0 +1,4 @@
+from . import help
+from . import menu_handlers
+from . import start
+from . import echo
